2fa432ad22007e6df2f76428554da2ab1e42df9a
Fernando Frazão da Silva <ffrazao@gmail.com>
2026-06-17 22:34:16 -0300

    Ajustes para o keycloak funcionar, alé de ajustes de volume no seaweedfs

--- seaweedfs/entrypoint.sh
+++ seaweedfs/entrypoint.sh
@@ -12,7 +12,7 @@ export PGPASSWORD="${S3_SEN}"
 psql -h "${POSTGRES_URL}" -U "${S3_USU}" -d "${S3_BD}" -c "
 CREATE TABLE IF NOT EXISTS filemeta (
   dirhash     BIGINT        NOT NULL,
-  name        VARCHAR(1000) NOT NULL,
+  name        VARCHAR(1000) COLLATE \"C\" NOT NULL,
   directory   TEXT          NOT NULL,
   meta        BYTEA         NOT NULL,
   PRIMARY KEY (dirhash, name)
@@ -23,4 +23,12 @@ CREATE INDEX IF NOT EXISTS idx_filemeta_dir ON filemeta (directory);
 echo "3. Iniciando o servidor SeaweedFS..."
 # O comando 'exec' substitui o processo do shell pelo processo do weed,
 # garantindo que o container trate os sinais de parada (SIGTERM) corretamente.
-exec weed server -dir=/data -s3 -s3.config=/etc/seaweedfs/s3.json -filer
+
+exec weed server \
+  -dir=/data \
+  -master.defaultReplication=000 \
+  -master.volumePreallocate=false \
+  -volume.max=10 \
+  -s3 \
+  -s3.config=/etc/seaweedfs/s3.json \
+  -filer
